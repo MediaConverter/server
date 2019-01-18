c93649da146e4a21e6a4f4bd7c5e710ad60d96f5
anharfathoni <anharaf28@gmail.com>
2019-01-18 11:02:01 +0700

    anhar

--- controllers/gif.js
+++ controllers/gif.js
@@ -1,5 +1,33 @@
 const axios = require('axios')
 
 module.exports = {
-  
+  convertToGif: function (req, res) {
+    let newGif = { fetchUrl, noMd5, cut, caption } = req.body
+    axios({
+      method: "POST",
+      url: 'https://api.gfycat.com/v1/gfycats',
+      data: newGif,
+    })
+    .then( ({data}) => {
+      res.status(200).json(data)
+    })
+    .catch( error => {
+      res.status(400).json(error)
+    })
+  },
+
+  getGif: function (req, res) {
+    let gifname = req.params.gifname
+
+    axios({
+      method: "GET",
+      url: `https://api.gfycat.com/v1/gfycats/${gifname}`
+    })
+    .then( ({data}) => {
+      res.status(200).json(data.gfyItem)
+    })
+    .catch( error => {
+      res.status(400).json(error)
+    })
+  }
 }
